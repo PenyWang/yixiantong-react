6c5317a3c24c60f7483b076a5dda3089074bc563
skyler <379522872@qq.com>
2020-05-25 19:19:54 +0800

    安装依赖

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+
+  fetch('http://example.com/movies.json')
+  .then(function(response) {
+    var a = '1111111';
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
